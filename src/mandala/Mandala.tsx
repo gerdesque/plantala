968ed7460d2e4aaed02a4af12aaa6a4cec76e3ae
Gerd Müller <gerdesk@posteo.org>
2020-11-22 12:58:30 +0100

    Ajust Mandala size and position (center mode)

--- src/mandala/Mandala.tsx
+++ src/mandala/Mandala.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { useCanvas } from './useCanvas';
 import { IPlant } from '../plant/Plant';
 
@@ -6,19 +7,27 @@ interface IMandalaProps {
   selectedPlants: IPlant[],
 }
 
+const useStyles = makeStyles(() => ({
+  canvas: {
+    width: '100%',
+    height: 'auto'
+  }
+}));
+
 const Mandala = ({ selectedPlants }: IMandalaProps) => {
+  const classes = useStyles();
   const { setPlants, canvasRef, canvasWidth, canvasHeight } = useCanvas();
 
-  const addPlants = () => {
+  useEffect(() => {
     setPlants(selectedPlants);
-  };
+  });
 
   return (
     <canvas 
       ref={canvasRef}
+      className={classes.canvas}
       width={canvasWidth}
       height={canvasHeight}
-      onClick={addPlants}
     />
   );
 };
--- src/mandala/useCanvas.tsx
+++ src/mandala/useCanvas.tsx
@@ -1,8 +1,29 @@
 import { useState, useEffect, useRef } from 'react';
 import { IPlant } from '../plant/Plant';
 
-const canvasWidth: number = 800; // TODO: Add window.innerWidth * .5
-const canvasHeight: number = 800; // TODO: Add window.innerHeight * .5
+const canvasWidth: number = 800;
+const canvasHeight: number = 800;
+
+// function drawCoordinateSystem (context:CanvasRenderingContext2D) {
+//   for (var x = 0.5; x < canvasWidth; x += 10) {
+//     context.moveTo(x, 0);
+//     context.lineTo(x, canvasWidth);
+//   }
+//   for (var y = 0.5; y < canvasHeight; y += 10) {
+//     context.moveTo(0, y);
+//     context.lineTo(canvasHeight, y);
+//   }
+//   context.strokeStyle = "#eee";
+//   context.stroke();
+
+//   context.beginPath();
+//   context.moveTo(0, canvasHeight/2);
+//   context.lineTo(canvasWidth, canvasHeight/2);
+//   context.moveTo(canvasWidth/2, 0);
+//   context.lineTo(canvasWidth/2, canvasHeight);
+//   context.strokeStyle = "#000";
+//   context.stroke();
+// }
 
 function drawPlants (context:CanvasRenderingContext2D, plant:IPlant, index: number) {
   
@@ -19,13 +40,9 @@ function drawPlants (context:CanvasRenderingContext2D, plant:IPlant, index: numb
 
   for (let num = 0; num < imageAmount; num ++) {
     const angle = (num / (imageAmount / 2)) * Math.PI;
-    const dx = (imageOrder * Math.cos(angle)) + (-imageSize * imageScale / 2);
-    const dy = (imageOrder * Math.sin(angle)) + (-imageSize * imageScale / 2);
+    const dx = (imageOrder * Math.cos(angle))
+    const dy = (imageOrder * Math.sin(angle))
 
-    // const iwM = imageSize * imageScale * 2 + canvasWidth;
-    // const ihM = imageSize * imageScale * 2 + canvasHeight;
-    // const xr = ((x % iwM) + iwM) % iwM - imageSize * imageScale;
-    // const yr = ((y % ihM) + ihM) % ihM - imageSize * imageScale;
     const xr = dx + canvasWidth / 2;
     const yr = dy + canvasHeight / 2;
     const r = imageRotation + step * num * Math.PI / 180;
@@ -33,13 +50,13 @@ function drawPlants (context:CanvasRenderingContext2D, plant:IPlant, index: numb
     const image = new Image();
 
     image.onload = () => {
-      // context.translate(canvasWidth / 2, canvasHeight / 2 );
-      // context.drawImage(image, dx, dy, imgX, imgY);
-
       //console.log(`name: ${plant.name}, num: ${num}, angle: ${angle}, imageSize: ${imageSize}, imageScale: ${imageScale}, rotation: ${imageRotation + step * num}, dx: ${dx}, dy: ${dy}, xr: ${xr}, yr: ${yr}`);
       context.setTransform(imageScale, 0, 0, imageScale, xr, yr);
       context.rotate(r);
       context.drawImage(image, -imageSize / 2, -imageSize / 2, imageSize, imageSize);
+
+      // context.lineWidth = 2;
+      // context.strokeRect(-imageSize / 2, -imageSize / 2, imageSize, imageSize);
     }
 
     image.src = `${process.env.PUBLIC_URL}/assets/${plant.source}`;
@@ -57,6 +74,9 @@ export function useCanvas(){
       if (context) {
         context.clearRect( 0,0, canvasWidth, canvasHeight );
         context.setTransform(1,0,0,1,0,0);
+
+        //drawCoordinateSystem(context);
+
         plants.forEach((plant, index) => {
           drawPlants(context, plant, index); 
         });
