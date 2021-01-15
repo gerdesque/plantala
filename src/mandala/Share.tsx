f09ec94bf649449986593bbadd3ad13c28b36b90
Gerd Müller <gerdesk@posteo.org>
2021-01-15 18:21:04 +0100

    Add print functionality

--- src/mandala/Share.tsx
+++ src/mandala/Share.tsx
@@ -9,9 +9,15 @@ interface IShareProps {
 const Share = ({ setPlantalaData, canvasRef }: IShareProps) => {
 
   return (
-    <Grid container justify="center">
-      <Button variant="contained" color="secondary"
-        onClick={() => setPlantalaData(canvasRef.current?.toDataURL())}>Download</Button>
+    <Grid container spacing={2} justify="center">
+      <Grid item>
+        <Button variant="contained" color="secondary"
+          onClick={() => setPlantalaData(canvasRef.current?.toDataURL())}>Download</Button>
+      </Grid>
+      <Grid item>
+        <Button variant="outlined" color="secondary"
+          onClick={() => window.print()}>Drucken</Button>
+      </Grid>
     </Grid>
   );
 };
